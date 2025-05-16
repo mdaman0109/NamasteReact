de40ec15ad77b1a3775a2fd8723f66f969ac28ff
Md Aman <aman.md0109@gmail.com>
2025-05-16 06:17:04 +0530

    LayingTheFoundation

--- 03_LayingTheFoundation/script.js
+++ 03_LayingTheFoundation/script.js
@@ -4,7 +4,15 @@ import React from "react";
 import ReactDOM from "react-dom/client"; 
 
 const newelement = <h1 id="heading">Hello from JSX</h1>
+const Newcomponent =() => (<h1>Hey from react component</h1>)
+const Secondcomponent =() => {return (<div>
+    <Newcomponent/>
+    {newelement}
+    <h1>Hey from second component</h1>
+</div>)}
+
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container); 
-root.render(newelement);
+root.render(<Secondcomponent/>);
+
